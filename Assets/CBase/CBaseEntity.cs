e538e19c54c9da7287b295f92e1cbd88fede0974
MMC-Scholars <latrunkster@gmail.com>
2017-09-26 14:06:31 -0400

    Initial commit - added Unity project which we'll use later on as a template for other projects - began outlining a base CBaseEntity MonoBehavior - Michael

---
+++ Assets/CBase/CBaseEntity.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets {
+	public partial class CBaseEntity : MonoBehaviour {
+
+		//This simple accessor will help us type less later on
+		public GameObject obj() { return gameObject; }
+
+		/*
+		 * Private variables for teleporting to original spawn location/angle
+		 */
+		private Vector3     m_vSpawnLocation;
+		private Quaternion  m_qSpawnAngle;
+
+
+		/**
+		 * Health functionality
+		 */
+		private int		m_iHealth;
+		public  int		m_iSpawnHealth = 100;
+		public	int		GetHealth() { return m_iHealth; }
+		//public	void	SetHealth(int iHealth) { m_iHealth = iHealth; }
+
+
+
+		/**
+		 * Respawn function
+		 */
+		public void Respawn() {
+			//reset position
+			obj().transform.SetPositionAndRotation(m_vSpawnLocation,m_qSpawnAngle);
+
+			//reset bitflags
+			m_iFlags = m_iSpawnFlags;
+
+			//reset health
+			m_iHealth = m_iSpawnHealth;
+		}
+
+		// Use this for initialization
+		void Start() {
+
+			//Remember default spawn values
+			m_iSpawnFlags		= m_iFlags;
+			m_vSpawnLocation	= obj().transform.position;
+			m_qSpawnAngle = obj().transform.rotation;
+		}
+
+		// Update is called once per frame
+		void Update() {
+
+		}
+	}
+}
---
+++ Assets/CBase/CBaseEntity_Flags.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets {
+	/**
+	 * Definitions for the bitflags used by CBaseEntity. They are defined separately here
+	 * for the sake of organization.
+	 * 
+	 */ 
+	public partial class CBaseEntity : MonoBehaviour {
+
+		/**
+		 * Flag accessors/setters
+		 */ 
+		public	int         m_iFlags = 0;
+		public	int			GetFlags() { return m_iFlags; }
+		public	void		SetFlags(int iFlags) { m_iFlags = iFlags; }
+
+		/**
+		 * The flags we originally spawned with
+		 */
+		private int         m_iSpawnFlags = 0;
+
+		/**
+		 * These are the bit flags
+		 */ 
+		public	static int  FL_INVINCIBLE       = 1 << 0,	//Clamp health to always be above one?
+							FL_NODAMAGE         = 1 << 1,	//Block all damage? (health stays at default value)
+							FL_TRIGGERIGNORE    = 1 << 2;	//Should MMC's triggers ignore this entity?
+
+		/**
+		 * Use this to check if an object has a specific flag
+		 */ 
+		public bool		HasFlag(int iFlag) {
+			return (m_iFlags & iFlag) > 0;		
+		}
+							
+	}
+}
