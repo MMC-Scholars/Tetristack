226c7885e9acfc215f0f60e7475a1766fd944474
MMC-Scholars <latrunkster@gmail.com>
2017-11-07 15:33:33 -0500

    Added CDoor base template, in progress. -Dom

---
+++ Assets/CBase/CDoor.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Assets
+{
+    class CDoor : CBaseEntity
+    {
+
+		public bool bOpen;
+		public bool bClosed;
+        public bool bInvisible;
+        public bool bWalkThrough;
+
+        public float flTimeToOpen;
+        public float flTimeToClose;
+
+        private GameObject door;
+
+
+		void OpenDoor(bool IsOpen, float TimeToOpen)
+        {
+			//Timer until TimeToOpen, then...
+			
+            IsOpen = true;
+        }
+
+		void CloseDoor(bool IsOpen, float TimeToClose)
+        {
+            //Timer until TimeToClose, then...
+
+            IsOpen = false;
+        }
+
+		//DestroyDoor()
+
+		
+
+
+
+
+
+
+
+    }
+}
