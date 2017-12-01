89bfe3589c885fc748618d2cd4caee027185d958
MMC-Scholars <latrunkster@gmail.com>
2017-12-01 14:03:17 -0500

    Began writing a script for a trigger which will detect when a block has fallen down - added missing material - Michael

---
+++ Assets/NTowerBuilder/CFallTrigger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UnityEngine;
+
+namespace Assets.NTowerBuilder {
+
+	abstract partial class g {
+		CFallTrigger pFallTrigger = null;
+	}
+
+	/**
+	 * Communicates to the gamerules how many blocks fall off the platform.
+	 */ 
+	class CFallTrigger : CBaseEntity {
+
+		private int m_iCount = 0;
+
+		public int FallenBlockCount() { return m_iCount; }
+
+		/************************************************************
+		 * Unity overrides
+		 ***********************************************************/ 
+		private void OnCollisionEnter(Collision collision) {
+			CBaseBlock blk = collision.collider.gameObject.GetComponent<CBaseBlock>();
+			if (blk != null && blk.isInstantiated()) {
+				m_iCount++;
+			}
+		}
+
+		private void OnCollisionExit(Collision collision) {
+			CBaseBlock blk = collision.collider.gameObject.GetComponent<CBaseBlock>();
+			if(blk != null && blk.isInstantiated()) {
+				CBaseBlock.Destroy(blk.obj());
+			}
+		}
+	}
+}
